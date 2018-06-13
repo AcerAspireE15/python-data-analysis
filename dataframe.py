bf97804e65db55c0894065f870911a40c93d4005
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-13 13:23:00 +0530

    dataframe

---
+++ dataframe.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[11]:
+
+
+data = {'Name':['John', 'Tim', 'Rob'], 
+        'Age':[34, 23, 42],
+        'Salary':[4500, 2300, 5800]
+       }
+
+
+# In[12]:
+
+
+from pandas import DataFrame
+
+frame = DataFrame(data)
+
+
+# In[13]:
+
+
+frame
+
+
+# In[14]:
+
+
+frame
+
