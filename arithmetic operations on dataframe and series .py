e1055ce0674f09d4cf57ca0c8573036859514fab
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-13 21:55:17 +0530

    arithmetic operations on dataframe and series

---
+++ arithmetic operations on dataframe and series .py
@@ -0,0 +1,75 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from pandas import Series
+Series1 = Series([1, 2, 3, 4, 5])
+Series2 = Series([100, 200, 300, 400, 500])
+Series1 + Series2
+
+
+# In[2]:
+
+
+from pandas import Series
+Series1 = Series([1, 2, 3, 4, 5])
+Series2 = Series([100, 200, 300, 400, 500, 600, 700])
+Series1 + Series2
+
+
+# In[3]:
+
+
+from pandas import Series
+Series1 = Series([1, 2, 3, 4, 5])
+Series2 = Series([100, 200, 300, 400, 500])
+Series1 - Series2
+
+
+# In[4]:
+
+
+from pandas import Series
+Series1 = Series([1, 2, 3, 4, 5])
+Series2 = Series([100, 200, 300, 400, 500])
+Series1 * Series2
+
+
+# In[5]:
+
+
+from pandas import Series
+Series1 = Series([1, 2, 3, 4, 5])
+Series2 = Series([100, 200, 300, 400, 500])
+Series1 / Series2
+
+
+# In[7]:
+
+
+from pandas import DataFrame
+data1 = {'Speed':[100, 103, 105],
+         'Temp':[34, 23, 42]}
+frame1 = DataFrame(data1)
+frame1
+
+
+# In[10]:
+
+
+from pandas import DataFrame
+data1 = {'Speed':[101, 113, 115],
+         'Temp':[34, 23, 42],
+         'Humidity':[45, 23, 58]
+        }
+frame2 = DataFrame(data1)
+frame2
+
+
+# In[11]:
+
+
+frame1 + frame2
+
