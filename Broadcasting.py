e037f71cfc9ce8a39b9e3486dddb1b96dfb5b676
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-14 15:33:15 +0530

    Broadcasting

---
+++ Broadcasting.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[28]:
+
+
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([10,20,30,40])
+c = a * b
+print(c)
+
+
+# In[29]:
+
+
+c = a + b
+print(c)
+
+
+# In[30]:
+
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+b = np.array([1,2,3])
+print(a)
+print(b)
+
+
+# In[31]:
+
+
+print(a+b)
+
